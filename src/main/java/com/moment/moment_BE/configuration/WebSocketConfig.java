3a1d591bffcfb99a122d4dcfe12dc45e4fa602f9
Duongg <lengocduong003@gmail.com>
2024-12-27 16:00:51 +0700

    fix cros

--- src/main/java/com/moment/moment_BE/configuration/WebSocketConfig.java
+++ src/main/java/com/moment/moment_BE/configuration/WebSocketConfig.java
@@ -34,7 +34,7 @@ public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns(allowedOrigin)
-                .addInterceptors(httpHandshakeInterceptor)
+//                .addInterceptors(httpHandshakeInterceptor)
                 .withSockJS();
 
     }
