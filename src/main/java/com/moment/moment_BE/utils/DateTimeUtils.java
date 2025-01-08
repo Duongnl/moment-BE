8593bd99f371ca67e5f674b14cbc05ce4c241ada
p1ck <107763946+Duongnl@users.noreply.github.com>
2025-01-09 02:07:37 +0700

    Update DateTimeUtils.java

--- src/main/java/com/moment/moment_BE/utils/DateTimeUtils.java
+++ src/main/java/com/moment/moment_BE/utils/DateTimeUtils.java
@@ -9,14 +9,14 @@ public class DateTimeUtils {
 
     public static LocalDateTime convertUtcToUserLocalTime(String utcTime) {
         ZonedDateTime utcZonedDateTime = ZonedDateTime.parse(utcTime, DateTimeFormatter.ISO_DATE_TIME);
-
+        System.out.println("clienttime duong >>> " + utcZonedDateTime);
         ZoneId userZoneId = ZoneId.of("Asia/Ho_Chi_Minh");
         return utcZonedDateTime.withZoneSameInstant(userZoneId).toLocalDateTime();
     }
 
     public static LocalDateTime getCurrentTimeInSystemLocalTime() {
         ZonedDateTime serverTime = ZonedDateTime.now(ZoneId.systemDefault());
-
+         System.out.println("serverTime duong >>> " + serverTime);
         ZoneId systemZoneId = ZoneId.of("Asia/Ho_Chi_Minh");
         return serverTime.withZoneSameInstant(systemZoneId).toLocalDateTime();
     }
